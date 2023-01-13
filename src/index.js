75ae8d3715f76a2c0d662f9ae0623d61ae774f3f
Firdavs <firdavseminovhpl@gmail.com>
2023-01-13 23:34:28 +0500

    small-projects-react

--- src/index.js
+++ src/index.js
@@ -2,19 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-let nav = {
-  nav : 'navbar',
-  section : 'section',
-}
-const goWork = 'Go work'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App title={'index'}
-      go={goWork}
-      navv={nav} 
-     />
+    <App/>
   </React.StrictMode>
 );
 
