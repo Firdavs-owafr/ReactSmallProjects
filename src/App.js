a8fe3c5ee22df08aff83fcfd2d04b5b8b3a67062
Firdavs <firdavseminovhpl@gmail.com>
2023-01-18 21:49:01 +0500

    api1

--- src/App.js
+++ src/App.js
@@ -1,20 +1,58 @@
-import React, { Component } from "react";
+import React from "react";
 import './index.css';
 
-class App extends Component {
+class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
     }
   }
 
+  componentDidMount(){
+    fetch('www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail',  {
+      method: "GET",
+      headers: {
+          'Content-Type': 'application/json;charset=utf-8'
+      },
+      body: JSON.stringify(this.state)
+  })
+      .then(res => res.json())
+      .then((json) => {
+        this.setState({
+          isLoaded: true,
+          items: json.drinks
+        })
+      },
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        })
+      }
+      )
+  }
+
   render() {
-    alert('render');
-    return (
-      <div>
-          <h1>hiiiiiiii</h1>
-      </div>
-    );
+    const {error, isLoaded, items} = this.state;
+    if(error){
+      return <h1>Error {error.message} </h1>
+    } else if (!isLoaded) {
+      return <h1>Loading... </h1>
+    } else {
+        return (
+          <ul>
+            {items.map((item) => (
+                <li key={item.name}>
+                    {item.strDrink}
+                    <img width='50' height="50" src={item.strDrinkThumb} alt="" />
+                </li>
+            ))}
+          </ul>
+        )
+    }
   }
 
 }
