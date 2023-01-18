e7a59549add625196b12e8d20377dd3f4e2040c6
Firdavs <firdavseminovhpl@gmail.com>
2023-01-18 19:16:38 +0500

    api1

--- src/App.js
+++ src/App.js
@@ -5,40 +5,14 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      class: 'off',
-      label: 'Pres'
     }
-    this.press = this.press.bind(this)
-    console.log('constructor'); 
-  }
-
-  componentDidMount(){
-    console.log('componentDidMount');
-  }
-
-  componentDidUpdate(){
-    console.log('componentDidUpdate');
-  }
-
-  shouldComponentUpdate(){
-    console.log('shouldComponentUpdate');
-    return true;
-  }
-
-  componentWillUnmount(){
-    console.log('componentWillUnmount');
-  }
-
-  press(){
-    var className = (this.state.class === 'off') ? 'on' : 'off';
-    this.setState({class: className})
   }
 
   render() {
-    console.log('render');
+    alert('render');
     return (
       <div>
-        <button onClick={this.press} className={this.state.class}>{this.state.label}</button>
+          <h1>hiiiiiiii</h1>
       </div>
     );
   }
