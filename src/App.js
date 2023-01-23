2ae9c5ed6a2d3cfd0ae0572f3db0d625c8898efe
Firdavs <firdavseminovhpl@gmail.com>
2023-01-23 21:08:26 +0500

    useeffect

--- src/App.js
+++ src/App.js
@@ -1,40 +1,7 @@
-import React, { Component } from "react";
+import React from "react";
 import { useState } from "react";
 import './index.css';
 
-// export default class App extends Component { 
-//   constructor(props){
-//     super(props)
-//     this.state = {
-//       counter: 0
-//     }
-//   }
-
-//   plus = () => {
-//     this.setState(prevState => ({counter: prevState.counter + 1}))
-//   }
-
-//   minus = () => {
-//     this.setState(prevState => ({counter: prevState.counter - 1}))
-//   }
-
-//   null = () => {
-//     this.setState(prevState => ({counter: 0}))
-//   }
-
-//   render(){
-//     return(
-//       <div>
-//         <h1>{this.state.counter}</h1>
-//         <div className="d-flex">
-//           <button onClick={this.plus} className="btn btn-success">+</button>
-//           <button onClick={this.minus} className="btn btn-danger">-</button>
-//           <button onClick={this.null} className='btn btn-dark'>0</button>
-//         </div>
-//       </div>
-//     )
-//   }
-// }
 
 export default function App () {
   const [counter,setCounter] = useState(0)
