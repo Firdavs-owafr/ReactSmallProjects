0c214c587113a6fd8b2541cff2666c9a8847061c
Firdavs <firdavseminovhpl@gmail.com>
2023-01-25 23:06:15 +0500

    projectHook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React,{ useRef,useState } from "react";
+import React, { useRef, useState } from "react";
 import './index.css';
 import './Boostrap.css'
 
-export default function App () {
-  const [state,setSate] = useState('')
-  const [state2,setSate2] = useState('')
+export default function App() {
+  const [state, setSate] = useState('')
+  const [state2, setSate2] = useState('')
   const cvcRed = useRef(null)
   const cvcRef2 = useRef(null)
 
@@ -12,7 +12,7 @@ export default function App () {
     const value = e.target.value;
     setSate(value)
     console.log(value.length);
-    if(value.length === 16){
+    if (value.length === 16) {
       cvcRed.current.focus()
     }
   }
@@ -20,17 +20,17 @@ export default function App () {
   const handle22 = (e) => {
     const value = e.target.value;
     setSate2(value)
-    if(value.length === 4){
+    if (value.length === 4) {
       cvcRef2.current.focus()
     }
   }
 
-  return(
-            <div>
-              <h1>hi</h1>
-                <input type="text" className="form-control " placeholder="Card Number" onChange={handle} value={state} />
-                <input ref={cvcRed} type="text" className="form-control" placeholder="Secure" onChange={handle22} value={state2} />
-                <input ref={cvcRef2} type="text" className="form-control" placeholder="Oppa" />
-            </div>
-)
+  return (
+    <div>
+      <h1>hi</h1>
+      <input type="text" className="form-control " placeholder="Card Number" onChange={handle} value={state} />
+      <input ref={cvcRed} type="text" className="form-control" placeholder="Secure" onChange={handle22} value={state2} />
+      <input ref={cvcRef2} type="text" className="form-control" placeholder="Oppa" />
+    </div>
+  )
 }
