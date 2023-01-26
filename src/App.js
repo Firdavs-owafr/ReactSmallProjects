5b1e538275ff9b8befd00c81009fea3c0f927fad
Firdavs <firdavseminovhpl@gmail.com>
2023-01-26 08:20:11 +0500

    useContext2

--- src/App.js
+++ src/App.js
@@ -1,27 +1,7 @@
-import React,{useState} from "react";
-import './index.css';
-import ActionsCard from "./ActionsCard";
-import StatusCard from "./StatusCard";
-import  Context  from "./Context";
+import React from 'react'
 
 export default function App() {
-    const [counter, setcounter] = useState(0)
-
-    const count = (n) => {
-      setcounter(counter + n)
-    }
-
-    const value = {
-      counter,
-      count
-    }
-
   return (
-    <Context.Provider value={value}>
-      <div className="container">
-        <StatusCard/>
-        <ActionsCard/>
-    </div>
-    </Context.Provider>
+    <div>App</div>
   )
 }
