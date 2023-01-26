cdbe1ebc02841bd3ba1429cb68fedfb946a67a26
Firdavs <firdavseminovhpl@gmail.com>
2023-01-26 10:24:08 +0500

    ReactRouterDOM

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
-import React,{ useState } from 'react'
-import { createContext } from 'react'
-import Child1 from './components/Child1'
-
-export const ContentText = createContext(0)
+import React from 'react'
 
 export default function App() {
-  const [counter, setCounter] = useState(0)
   return (
-    <ContentText.Provider value={counter}>
-        <h1>{counter}</h1>
-        <button onClick={() => setCounter(counter + 1)}>+</button>
-        <Child1 coun={counter} /> 
-    </ContentText.Provider>
+    <div>App</div>
   )
 }
